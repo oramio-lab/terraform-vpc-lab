affe9464f6586ea239efcc3c5722f80b68bd9c38
oramio-lab <oramio@naver.com>
2026-05-02 00:27:49 +0900

    Update main.tf
    sg

--- main.tf
+++ main.tf
@@ -53,13 +53,45 @@ resource "aws_route_table_association" "rta" {
   route_table_id = aws_route_table.rt.id
 }
 resource "aws_instance" "web" {
-  ami           = "ami-0c94855ba95c71c99"  # (us-east-1 기준 Amazon Linux2)
+  ami           = "ami-0c94855ba95c71c99"
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public.id
 
   associate_public_ip_address = true
 
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
   tags = {
     Name = "terraform-ec2"
   }
+}
+
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Allow SSH"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]   # 실습용 (강의에서 꼭 설명!)
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web-sg"
+  }
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "terraform-key"
+  public_key = file("~/.ssh/id_rsa.pub")
 }
